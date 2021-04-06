7dfdb38f7bff94ea437d6b2815ca890780322869
Tyler-Jay-Stevahn <tylerjay1996@hotmail.com>
2021-04-06 08:19:41 -0500

    changed name and age

--- variables.c
+++ variables.c
@@ -2,9 +2,9 @@
 #include<stdlib.h>
 
 int main(){
-    printf("There was once a man named George.\n");
-    printf("He was 70 years old.\n");
-    printf("He really liked the name George.\n");
-    printf("But did not like being 70.\n");
+    printf("There was once a man named John.\n");
+    printf("He was 35 years old.\n");
+    printf("He really liked the name John.\n");
+    printf("But did not like being 35.\n");
     return 0;
 }
