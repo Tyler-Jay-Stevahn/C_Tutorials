e270aac77ca99b6d774d9917455057b32e0eb7fd
Tyler-Jay-Stevahn <tylerjay1996@hotmail.com>
2021-04-06 08:18:50 -0500

    initial variables file

---
+++ variables.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    printf("There was once a man named George.\n");
+    printf("He was 70 years old.\n");
+    printf("He really liked the name George.\n");
+    printf("But did not like being 70.\n");
+    return 0;
+}
